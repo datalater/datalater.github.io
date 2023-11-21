4614e3af5f09ddff48c6467afce6549525f0ddab
datalater <the7mincheol@gmail.com>
2023-11-21 18:02:30 +0900

    Initial commit

---
+++ pages/_document.js
@@ -0,0 +1,39 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    const meta = {
+      title: "Min",
+      description: "Min on software",
+      image: "/favicon.ico",
+    };
+
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="robots" content="follow, index" />
+          <meta name="description" content={meta.description} />
+          <meta property="og:site_name" content={meta.title} />
+          <meta property="og:description" content={meta.description} />
+          <meta property="og:title" content={meta.title} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@min" />
+          <meta name="twitter:title" content={meta.title} />
+          <meta name="twitter:description" content={meta.description} />
+          <link rel="icon" type="image/x-icon" href={meta.image} />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
