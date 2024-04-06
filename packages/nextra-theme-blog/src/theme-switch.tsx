419688071c81072040da58304913325b8d27c5f3
datalater <the7mincheol@gmail.com>
2024-04-06 16:29:01 +0900

    style: add user-select: none to theme-switch
    Format too

--- packages/nextra-theme-blog/src/theme-switch.tsx
+++ packages/nextra-theme-blog/src/theme-switch.tsx
@@ -1,36 +1,36 @@
-import { useTheme } from 'next-themes';
-import { useMounted } from 'nextra/hooks';
-import { MoonIcon, SunIcon } from 'nextra/icons';
-import useSound from 'use-sound';
-import switchOn from './assets/sounds/switch-on.mp3';
-import switchOff from './assets/sounds/switch-off.mp3';
+import { useTheme } from 'next-themes'
+import { useMounted } from 'nextra/hooks'
+import { MoonIcon, SunIcon } from 'nextra/icons'
+import useSound from 'use-sound'
+import switchOn from './assets/sounds/switch-on.mp3'
+import switchOff from './assets/sounds/switch-off.mp3'
 
 export default function ThemeSwitch() {
-  const { setTheme, resolvedTheme } = useTheme();
-  const mounted = useMounted();
-  const isDark = resolvedTheme === 'dark';
+  const { setTheme, resolvedTheme } = useTheme()
+  const mounted = useMounted()
+  const isDark = resolvedTheme === 'dark'
 
-  const [playSwitchOn] = useSound(switchOn);
-  const [playSwitchOff] = useSound(switchOff);
+  const [playSwitchOn] = useSound(switchOn)
+  const [playSwitchOff] = useSound(switchOff)
 
   // @TODO: system theme
   const toggleTheme = () => {
-    setTheme(isDark ? 'light' : 'dark');
-    isDark ? playSwitchOn() : playSwitchOff();
-  };
+    setTheme(isDark ? 'light' : 'dark')
+    isDark ? playSwitchOn() : playSwitchOff()
+  }
 
   return (
     <span
       role="button"
       aria-label="Toggle Dark Mode"
-      className="nx-cursor-pointer nx-p-2 nx-text-current"
+      className="nx-cursor-pointer nx-p-2 nx-text-current nx-select-none"
       tabIndex={0}
       onClick={toggleTheme}
-      onKeyDown={(e) => {
-        if (e.key === 'Enter') toggleTheme();
+      onKeyDown={e => {
+        if (e.key === 'Enter') toggleTheme()
       }}
     >
       {mounted && isDark ? <MoonIcon /> : <SunIcon />}
     </span>
-  );
+  )
 }
